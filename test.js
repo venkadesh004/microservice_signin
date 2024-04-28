2d485b8737bec5e6a09df092dad98d1d50ba733f
venkadesh004 <venkadeshdeveloper@gmail.com>
2024-04-28 06:48:43 +0530

    update

--- test.js
+++ test.js
@@ -3,11 +3,11 @@ const {assert} = require('chai');
 
 describe('GET /', () => {
     it('should return the login page', async () => {
-        const response = await request("http://localhost:3000").get('/');
+        const response = await request("https://microservice-signin.onrender.com").get('/');
         assert.ok(response.status === 200);
     });
     it('should return the item page', async () => {
-        const response = await request("http://localhost:3000").post('/').send({
+        const response = await request("https://microservice-signin.onrender.com").post('/').send({
             username: 'venkadesh',
             password: 'venkadesh'
         });
